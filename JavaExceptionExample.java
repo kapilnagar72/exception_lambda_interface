243688d2a90df74ab9077597f46fc4d0d5e6c8b9
kapilnagar72 <kapilnagar14820@gmail.com>
2024-04-12 13:06:42 +0530

    JavaExceptionExample

---
+++ JavaExceptionExample.java
@@ -0,0 +1,16 @@
+package exception_lambda_interface;
+
+//JavaExceptionExample
+public class JavaExceptionExample {
+    
+    public static void main(String args[]) {
+        try {
+            // code that may raise exception
+            int data = 100 / 0;
+        } catch (ArithmeticException e) {
+            System.out.println(e);
+        }
+        // rest code of the program
+        System.out.println("rest of the code...");
+    }
+}
